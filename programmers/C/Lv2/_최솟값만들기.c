d22b538c29cd7bf88a6991a4940b40f79853c6b9
seondeok Kim <76850241+Seondeok-Kim@users.noreply.github.com>
2026-01-07 23:22:10 +0900

    Create _최솟값만들기.c

---
+++ programmers/C/Lv2/_최솟값만들기.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// A_len은 배열 A의 길이입니다.
+// B_len은 배열 B의 길이입니다.
+int cmp_asc(const void *a, const void *b)
+{
+    int x = *(int*)a;
+    int y = *(int*)b;
+    
+    if (x < y) return -1;
+    if (x > y) return 1;
+    return 0;
+}
+
+int solution(int A[], size_t A_len, int B[], size_t B_len) {
+    int answer = 0;
+    qsort(A, A_len, sizeof(int), cmp_asc);
+    qsort(B, B_len, sizeof(int), cmp_asc);
+    
+    for(int i = 0 ; i < A_len ; ++i)
+    { 
+        answer += A[i] * B[A_len - (i + 1)];
+    }
+    
+    return answer;
+}
