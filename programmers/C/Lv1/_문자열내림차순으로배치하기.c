93a7edc99e563e867c960a39f2075949d1d58037
seondeok Kim <76850241+Seondeok-Kim@users.noreply.github.com>
2026-01-14 14:14:55 +0900

    Update _문자열내림차순으로배치하기.c

--- programmers/C/Lv1/_문자열내림차순으로배치하기.c
+++ programmers/C/Lv1/_문자열내림차순으로배치하기.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
-int cmp_desc(const void *a, const void *b)
+int cmp_desc(const void* a, const void* b)
 {
     const char c1 = *(const char*) a;
     const char c2 = *(const char*) b;
-    return c2 - c1;
+    if(c1 > c2) return -1;
+    else if (c1 < c2) return 1;
+    return 0;
 }
 
-char* solution(const char* s) {
+char* solution(const char* s) {    
     int len = strlen(s);
-    char* answer = (char*)malloc(s + 1);
+    char* answer = (char*)malloc(len+1);
     strcpy(answer, s);
     qsort(answer, len, sizeof(char), cmp_desc);
-    
     return answer;
 }
