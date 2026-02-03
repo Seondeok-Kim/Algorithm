bf31036ac161f4ec2e7b841b817053da4d97339f
seondeok Kim <76850241+Seondeok-Kim@users.noreply.github.com>
2026-02-03 23:55:50 +0900

    Update _문자열나누기.c

--- programmers/C/Lv1/_문자열나누기.c
+++ programmers/C/Lv1/_문자열나누기.c
@@ -45,3 +45,48 @@ int solution(const char* s) {
     
     return answer;
 }
+
+
+
+
+
+
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+int solution(const char* s) {
+    int answer = 0;
+    int len = strlen(s);
+    
+    char x = s[0]; // x로 지정할 문자
+    int x_idx = 0; // x 시작 인덱스
+    
+    int cnt_x = 0; // x와 동일한 문자 개수
+    int cnt_nx = 0; // x와 다른 문자 개수
+    
+    for(int i = 0 ; s[i] != '\0'; ++i)
+    {
+        if(s[i] == x)
+        {
+            cnt_x++;
+        }
+        else
+        {
+            cnt_nx++;
+        }
+        
+        if(cnt_x == cnt_nx)
+        {
+            cnt_x = 0;
+            cnt_nx = 0;
+            answer++;
+            x = s[i+1]; 
+        }
+    }
+    if(cnt_x != cnt_nx) answer++;
+    
+    return answer;
+}
